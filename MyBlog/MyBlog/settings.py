a874a3b37c606a3d72ec97e45a12b1d92ce6fab1
Umair Mirza <mirzaumair5530@gmail.com>
2020-11-01 17:50:12 +0500

    Solve privacy

--- MyBlog/MyBlog/settings.py
+++ MyBlog/MyBlog/settings.py
@@ -136,8 +136,9 @@ MEDIA_ROOT=os.path.join(STATIC_DIR,'images')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('User_Email')
+EMAIL_PORT = 587
 EMAIL_HOST_PASSWORD = os.environ.get('User_Password')
+EMAIL_HOST_USER = os.environ.get('User_Email')
+
 
